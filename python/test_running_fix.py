2ab78e8043d0dbd674ae71d0d4ee6627accc3582
cmarchand <cedric.marchand@univ-ubs.fr>
2023-06-20 22:56:51 +0200

    test running fix with multiple point

--- python/test_running_fix.py
+++ python/test_running_fix.py
@@ -28,3 +28,43 @@ plt.show()
 
 
 # %%
+
+sigma = 0 # 2 degree
+
+amer1 = nav.Amer(100.0, 500.0)
+amer2 = nav.Amer(250.0, 510.0)
+amer3 = nav.Amer(500.0, 500.0)
+amer4 = nav.Amer(500.0, 100.0)
+amer5 = nav.Amer(200.0, 100.0)
+amer6 = nav.Amer(50.0, 150.0)
+
+amer1.plot_position()
+amer2.plot_position()
+amer3.plot_position()
+amer4.plot_position()
+amer5.plot_position()
+amer6.plot_position()
+amer_table=[amer1, amer2, amer3, amer4, amer5, amer6]
+
+
+boat = nav.Boat(200,300, course=np.pi/2, speed=100)
+boat.plot_position()
+boat.plot_speed()
+
+# build cost table such that lowest cost is + or - pi/2 angle
+bearing_table = np.zeros((len(amer_table),1),dtype=float)
+for i, amer in enumerate(amer_table):
+    amer.compute_bearing(boat,sigma)
+    bearing_table[i]=amer.bearing
+
+bearing_table = boat.course - bearing_table
+print(bearing_table)
+# bearing_table = abs(bearing_table)
+# print(bearing_table)
+#bearing_table = bearing_table - np.pi/4
+#print(bearing_table)
+
+plt.show()
+
+
+# %%
