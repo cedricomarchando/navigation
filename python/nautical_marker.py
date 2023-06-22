e21b120113114312c0adeeb7bd41508cfe3c5d6a
cmarchand <cedric.marchand@univ-ubs.fr>
2023-06-22 18:09:56 +0200

    add plot_land_marks

--- python/nautical_marker.py
+++ python/nautical_marker.py
@@ -1,9 +1,29 @@
 # %%
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
+import matplotlib.transforms as transforms
 
+def plot_land_marks(position_x, position_y, markersize, type):
+    """ plot land_marks"""
+    match type:
+        case 'lighthouse':
+            marker = Path.unit_regular_star(5,0.5)
+            facecolor='k'
+        case 'tower':
+            marker = build_land_tower_path(10,2)
+            facecolor='none'
+        case 'water_tower':
+            marker = build_water_tower_path(10,2)
+            facecolor='none'
+        case _:
+            print('notdefined landmark')
+            
+    plt.plot(position_x, position_y, marker=marker, linestyle='solid',
+            markerfacecolor=facecolor, markeredgecolor='k',
+            markersize=markersize, label=type)
+    
 
-def plot_nautical_symbol(position_x, position_y, markersize, shape_type, top_mark_type):
+def plot_sea_marks(position_x, position_y, markersize, shape_type, top_mark_type, floating):
     """ plot nautical symbol """
     
     shape_height = 12
@@ -11,9 +31,16 @@ def plot_nautical_symbol(position_x, position_y, markersize, shape_type, top_mar
     color = select_color(top_mark_type)
     
     width, shape_marker = select_shape(shape_type, shape_height)
-    topmark_marker = select_topmark_marker(top_mark_type, topmark_size, shift_up=shape_height + 2)
-    symbol_marker = Path.make_compound_path(shape_marker, topmark_marker)
+    if (top_mark_type  == 'green_no_top') or (top_mark_type  == 'red_no_top'):
+        symbol_marker = shape_marker
+        markersize = markersize/2
+    else:
+        topmark_marker = select_topmark_marker(top_mark_type, topmark_size, shift_up=shape_height + 2)
+        symbol_marker = Path.make_compound_path(shape_marker, topmark_marker)
     
+    if floating:
+        symbol_marker = symbol_marker.transformed(transforms.Affine2D().rotate(-0.3))
+
     plt.plot(position_x, position_y, marker=symbol_marker, linestyle='solid',
             markerfacecolor=color, markeredgecolor='k',
             markersize=markersize, label=top_mark_type)
@@ -26,7 +53,11 @@ def select_color(top_mark_type):
     match top_mark_type:
         case 'green':
             color = 'green'
-        case 'red':
+        case 'green_no_top':
+            color = 'green'
+        case 'red' :
+            color = 'red'
+        case 'red_no_top' :
             color = 'red'
         case 'special':
             color ='yellow'
@@ -137,6 +168,22 @@ def build_tower_path(height, width):
     tower_path = Path(vertices, codes)
     return tower_path
 
+def build_land_tower_path(height, width):
+    """ build tower path """
+    botton_tower_path = build_tower_path(height*3/4,width)
+    top_tower_path = build_rectangle_path(height/8,width/2,height*3/4)
+    land_tower_path = Path.make_compound_path(botton_tower_path, top_tower_path)
+    return land_tower_path
+
+def build_water_tower_path(height, width):
+    """ build tower path """
+    botton_tower_path = build_tower_path(height*3/4,width)
+    top_tower_path = build_rectangle_path(height/8,width,height*3/4)
+    land_tower_path = Path.make_compound_path(botton_tower_path, top_tower_path)
+    return land_tower_path
+
+
+
 def build_spherical(width):
     """ Build spherical path"""
     tmp = Path.arc(-30,210)
@@ -225,21 +272,46 @@ def danger_topmark(size, shift_up):
     
 if __name__ == "__main__":
     
-    topmark_type_list =['green','red','north','south','east','west','danger','special']
+    topmark_type_list =['green','red','north','south','east','west','danger','special','green_no_top','red_no_top']
     shape_type_list =['conical','can','spherical','spar','pillar','tower']
     MARKERSIZE=50
     
+    plt.figure(1)
     for j, shape in enumerate(shape_type_list):
         plt.text(1,j*2,shape, horizontalalignment='center')
     
     for i, topmark in enumerate(topmark_type_list):
         plt.text(i+2,len(shape_type_list)*2, topmark, horizontalalignment='center')
         for j, shape in enumerate(shape_type_list):
-            plot_nautical_symbol(i+2, j*2, MARKERSIZE, shape_type=shape, top_mark_type=topmark)
+            plot_sea_marks(i+2, j*2, MARKERSIZE, shape_type=shape, top_mark_type=topmark, floating=False)
+
+    plot_circle_line(1, 16, 2, 4)
+    plot_land_marks(1, 15, MARKERSIZE/4, 'lighthouse')
+    plot_land_marks(2, 15, MARKERSIZE, 'tower')
+    plot_land_marks(3, 15, MARKERSIZE, 'water_tower')
+    
+    plt.axis('off')
+    plt.xlabel('topmark')
+    plt.ylabel('shape')
+    plt.title('Nautical fixed mark symbols')
+    
+    plt.show()
+    """
+    plt.figure(2)
+    for j, shape in enumerate(shape_type_list):
+        plt.text(1,j*2,shape, horizontalalignment='center')
+    
+    for i, topmark in enumerate(topmark_type_list):
+        plt.text(i+2,len(shape_type_list)*2, topmark, horizontalalignment='center')
+        for j, shape in enumerate(shape_type_list):
+            plot_sea_marks(i+2, j*2, MARKERSIZE, shape_type=shape, top_mark_type=topmark, floating=True)
 
     plot_circle_line(1, 12, 2, 4)
     plt.axis('off')
     plt.xlabel('topmark')
     plt.ylabel('shape')
-    plt.title('Nautical symbols')
+    plt.title('Nautical floating mark symbols')
+    
     plt.show()
+    """
+    
