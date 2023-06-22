9404b4e32065c21fb1da94444bd999e2c74e02d9
cmarchand <cedric.marchand@univ-ubs.fr>
2023-06-22 11:10:11 +0200

    module for nautical symbols

---
+++ python/nautical_marker.py
@@ -0,0 +1,166 @@
+# %%
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+
+
+def plot_nautical_symbol(position_x, position_y, shape_type, top_mark_type):
+    """ plot nautical symbol """
+    
+    shape_height = 10
+    shape_width = 2
+    topmark_size = 2
+    color = select_color(top_mark_type)
+    shape_marker = build_rectangle_path(shape_height,shape_width,0)
+    #topmark_marker = green_topmark( size=topmark_size, height=shape_height +1)
+    topmark_marker = select_topmark_marker(top_mark_type, topmark_size, shift_up=shape_height + 2)
+    symbol_marker = Path.make_compound_path(shape_marker, topmark_marker)
+    
+    plt.plot(position_x, position_y, marker=symbol_marker, linestyle='solid',
+            markerfacecolor=color, markeredgecolor='k',
+            markersize=50, label=top_mark_type)
+    plot_circle_line(position_x, position_y, 1.5 , 4)
+
+def select_color(top_mark_type):
+    """ select color """
+    match top_mark_type:
+        case 'green':
+            color = 'green'
+        case 'red':
+            color = 'red'
+        case _:
+            color = 'black'
+    return color
+
+def select_topmark_marker(top_mark_type, size, shift_up):
+    """ select topmark marker """
+    match top_mark_type:
+        case 'green':
+            topmark_marker = green_topmark(size, shift_up)
+        case 'red':
+            topmark_marker = red_topmark(height=size*2, width=size*2, shift_up=shift_up)
+        case 'south':
+            topmark_marker = south_topmark(size, shift_up)
+        case 'north':
+            topmark_marker = north_topmark(size, shift_up)
+        case 'east':
+            topmark_marker = east_topmark(size, shift_up)
+        case 'west':
+            topmark_marker = west_topmark(size, shift_up)
+        case 'danger':
+            topmark_marker = danger_topmark(size, shift_up)
+        case _:
+            print(' Not a valid topmark')
+    return topmark_marker
+            
+
+
+def build_triangle_path(size, shift_up):
+    """ Build a triangle path """
+    vertices = [(-size, shift_up), (0, shift_up + 2*size), (size, shift_up), (-size, shift_up)]
+    codes = [1, 2, 2, 79]
+    triangle_path = Path(vertices,codes)
+    return triangle_path 
+
+def build_triangle_down_path(size, shift_up):
+    """ Build a triangle path """
+    vertices = [(0, shift_up), (-size, shift_up + 2*size), (size, shift_up + 2*size), (0, shift_up)]
+    codes = [1, 2, 2, 79]
+    triangle_path = Path(vertices,codes)
+    return triangle_path 
+
+
+def build_rectangle_path(height, width, shift_up):
+    """ Build a rectangle path """
+    vertices = [(-width/2, shift_up), (-width/2, height + shift_up), 
+                (width/2, height + shift_up), (width/2, shift_up), (-width/2, shift_up)]
+    codes = [1, 2, 2, 2, 79]
+    rectangle_path = Path(vertices,codes)
+    return rectangle_path
+
+def build_line_path(start,stop):
+    """ Build line path """
+    vertices =[(0,start),(0,stop)]
+    codes = [1,2]
+    line_path = Path(vertices,codes)
+    return line_path
+
+def build_h_line_path(start,stop):
+    """ Build horizontal line path """
+    vertices =[(start,0),(stop,0)]
+    codes = [1,2]
+    line_path = Path(vertices,codes)
+    return line_path
+
+def build_circle_path(size, shift_up):
+    """ Build circle path """
+    circle = Path.circle([0.0, shift_up], size)
+    return circle
+
+def plot_circle_line(position_x, position_y,circle_size, line_size):
+    """ Build point path """
+    line_path1 = build_h_line_path(-line_size, -circle_size)
+    circle_path = build_circle_path(circle_size,0)
+    line_path2 = build_h_line_path(line_size, circle_size)
+    marker = Path.make_compound_path(line_path1, circle_path, line_path2)
+    plt.plot(position_x, position_y, marker=marker, linestyle='solid',
+            markerfacecolor='None', markeredgecolor='k',
+            markersize=10, label='Green Beacon')
+    
+def green_topmark(size, shift_up):
+    """ plot green beacon """
+    triangle_marker = build_triangle_path(size,shift_up)
+    return triangle_marker
+
+    
+def red_topmark(height, width, shift_up):
+    """ Plot red beacon """
+    red_marker = build_rectangle_path(height,width,shift_up)
+    return red_marker
+    
+def north_topmark(size, shift_up):
+    """ Plot north beacon """
+    triangle_marker1 = build_triangle_path(size, shift_up)
+    triangle_marker2 = build_triangle_path(size, shift_up + 2*size + 2)
+    north_marker = Path.make_compound_path(triangle_marker1, triangle_marker2)
+    return north_marker
+
+def south_topmark(size, shift_up):
+    """ Plot north beacon """
+    triangle_marker1 = build_triangle_down_path(size, shift_up)
+    triangle_marker2 = build_triangle_down_path(size, shift_up + 2*size + 2)
+    south_marker = Path.make_compound_path(triangle_marker1, triangle_marker2)
+    return south_marker
+
+def east_topmark(size, shift_up):
+    """ Plot east beacon """
+    triangle_marker1 = build_triangle_down_path(size, shift_up)
+    triangle_marker2 = build_triangle_path(size, shift_up + 2*size + 2)
+    east_marker = Path.make_compound_path(triangle_marker1, triangle_marker2)
+    return east_marker
+
+def west_topmark(size, shift_up):
+    """ Plot west beacon """
+    triangle_marker1 = build_triangle_path(size, shift_up)
+    triangle_marker2 = build_triangle_down_path(size, shift_up + 2*size + 2)
+    west_marker = Path.make_compound_path(triangle_marker1, triangle_marker2)
+    return west_marker
+    
+def danger_topmark(size, shift_up):
+    """ Plot danger beacon """
+    circle_marker1 = build_circle_path(size, shift_up + size)
+    circle_marker2 = build_circle_path(size, shift_up + 3*size + 2)
+    danger_marker = Path.make_compound_path(circle_marker1, circle_marker2)
+    return danger_marker
+    
+if __name__ == "__main__":
+    
+    plot_nautical_symbol(4, 10, shape_type='pilar', top_mark_type='green')
+    plot_nautical_symbol(5, 10, shape_type='pilar', top_mark_type='red')
+    plot_nautical_symbol(6, 10, shape_type='pilar', top_mark_type='north')
+    plot_nautical_symbol(7, 10, shape_type='pilar', top_mark_type='south')
+    plot_nautical_symbol(8, 10, shape_type='pilar', top_mark_type='east')
+    plot_nautical_symbol(9, 10, shape_type='pilar', top_mark_type='west')
+    plot_nautical_symbol(10, 10, shape_type='pilar', top_mark_type='danger')
+    
+    plot_circle_line(11, 10, 2, 4)
+    plt.show()
