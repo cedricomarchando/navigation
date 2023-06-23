02f69c17d58fa40ec01f49f760615676f10ac49b
cmarchand <cedric.marchand@univ-ubs.fr>
2023-06-23 16:42:29 +0200

    rename amer to mark

--- python/navigation.py
+++ python/navigation.py
@@ -38,80 +38,73 @@ class Boat:
     
     def plot_boat(self):
         """ plot with a boat marker in the direction of the course """
-        vertices = [(0, 0), (-1, 1), (1, 1), (4, 0), (1, -1), (-1, -1), (0, 0)]
         vertices = [(-2, 1), (1, 2), (3, 0), (1, -2), (-2, -1), (-2, 1)]
-        codes = [
-            Path.MOVETO, #begin the figure in the lower right
-            Path.CURVE3, #start a 3 point curve with the control point in lower left
-            Path.LINETO, #end curve in the upper left
-            Path.CURVE3, #start a new 3 point curve with the upper right as a control point
-            Path.LINETO, #end curve in lower right
-            Path.LINETO, #end curve in lower right
-        ]
+        codes = [1,3,2,3,1,79]
         boat_marker = Path(vertices,codes)
-        if self.course is not None:        
+        if self.course is not None:
             angle = self.course - np.pi/2
             boat_marker = boat_marker.transformed(transforms.Affine2D().rotate(-angle))
-        plt.plot(self.true_x, self.true_y, marker=boat_marker, markersize=30, color='blue')
+        plt.plot(self.true_x, self.true_y, marker=boat_marker,
+            markersize=10, color='blue',  markerfacecolor='none',
+            linestyle = 'None', label='Boat position')
         if self.estimate_x is not None:
-            plt.plot(self.estimate_x, self.estimate_y, marker=boat_marker, markersize=30, color='red')
+            plt.plot(self.estimate_x, self.estimate_y, marker=boat_marker,
+                markersize=10, color='red', markerfacecolor='none',
+                linestyle = 'None', label='Estimate')
 
-    
     def set_position(self,position):
         """ Set bot with new position """
         self.true_x = position[0]
         self.true_y = position[1]
-        
+
     def set_estimate(self,estimate):
         """ Set bot with new position """
         self.estimate_x = estimate[0]
         self.estimate_y = estimate[1]
-        
-    def run_fix(self,amer,duration,sigma):
-        """ Run fix: get position from 1 amer and speed """
-        save_bearing = amer.bearing
-        # compute amer bearing after running
+
+    def run_fix(self,mark,duration,sigma):
+        """ Run fix: get position from 1 mark and speed """
+        save_bearing = mark.bearing
+        # compute mark bearing after running
         self.run(duration)
-        amer.compute_bearing(self, sigma)
-        amer.plot_amer_bearing(self)
-        # run amer in the direction of the boat
+        mark.compute_bearing(self, sigma)
+        mark.plot_mark_bearing(self)
+        # run mark in the direction of the boat
         duration = 1
-        amer_tmp = Amer(
-            amer.position_x + self.speed * duration * np.sin(self.course),
-            amer.position_y + self.speed * duration * np.cos(self.course),
+        mark_tmp = Mark(
+            mark.position_x + self.speed * duration * np.sin(self.course),
+            mark.position_y + self.speed * duration * np.cos(self.course),
             save_bearing
             )
-        plt.plot(amer_tmp.position_x, amer_tmp.position_y,'+k', label ="Amer shifted")
+        plt.plot(mark_tmp.position_x, mark_tmp.position_y,'+k', label ="mark shifted")
 
-        amer_tmp.plot_amer_bearing(self)
+        mark_tmp.plot_mark_bearing(self)
 
-        estimate = compute_intersection(amer,amer_tmp)
-        del amer_tmp
+        estimate = compute_intersection(mark,mark_tmp)
+        del mark_tmp
         return estimate
 
 
-
-class Amer:
-    """ Amer class """
+class Mark:
+    """ Mark class, including landmarks and Seamarks """
     def __init__(self,position_x,position_y,bearing = None, name = None):
         self.position_x = position_x
         self.position_y = position_y
         self.bearing = bearing
         self.name = name
-        
+
     def plot_position(self):
         """ Plot position"""
-        plt.plot(self.position_x, self.position_y,'*k', markersize = 10, label ="Amer")
-    
-       
-    def plot_amer_bearing(self,boat):
-        """ Plot LOP of an amer with dotted line"""
+        plt.plot(self.position_x, self.position_y,'*k', markersize = 10, label ="Mark")
+
+    def plot_mark_bearing(self,boat):
+        """ Plot LOP of a mark with dotted line"""
         x_line = np.linspace(self.position_x,boat.true_x,10)
         y_line = x_line * 1/np.tan(self.bearing) + self.position_y - self.position_x *  1/np.tan(self.bearing)
         plt.plot(x_line, y_line, '--k', linewidth=0.5, label = "Line of Position (LoP)")
 
     def compute_bearing(self,boat,sigma):
-        """ compute Bearing angle of an Amer from the point of vie of the Boat """
+        """ compute Bearing angle of a mark from the point of vie of the Boat """
         vector_x = self.position_x - boat.true_x
         vector_y = self.position_y - boat.true_y
         bearing = np.arctan2(vector_x, vector_y)
@@ -120,27 +113,27 @@ class Amer:
         print(f"x={vector_x},y={vector_y}, bearing = {bearing}")
 
 
-def compute_intersection(amer1,amer2):
-    """ Compute intersection between two LOP of amer1 and amer2"""
-    # y = a1 x+ b1 (for LOP of amer1)
-    # y = a2 x+ b2 (for LOP of amer2)
+def compute_intersection(mark1,mark2):
+    """ Compute intersection between two LOP of mark1 and mark2"""
+    # y = a1 x+ b1 (for LOP of mark1)
+    # y = a2 x+ b2 (for LOP of mark2)
     #  thus intersection at x = (b1-b2)/(a2-a1)
-    intersection_x = ((amer1.position_y - 1/np.tan(amer1.bearing)*amer1.position_x)
-            - (amer2.position_y -1/np.tan(amer2.bearing)*amer2.position_x)) / ((1/np.tan(amer2.bearing)) - ( 1/np.tan(amer1.bearing)))
-    intersection_y = 1/np.tan(amer1.bearing) * intersection_x + amer1.position_y - 1/np.tan(amer1.bearing)*amer1.position_x
+    intersection_x = ((mark1.position_y - 1/np.tan(mark1.bearing)*mark1.position_x)
+            - (mark2.position_y -1/np.tan(mark2.bearing)*mark2.position_x)) / ((1/np.tan(mark2.bearing)) - ( 1/np.tan(mark1.bearing)))
+    intersection_y = 1/np.tan(mark1.bearing) * intersection_x + mark1.position_y - 1/np.tan(mark1.bearing)*mark1.position_x
     intersection = np.array([intersection_x, intersection_y])
     return intersection 
 
 
-def compute_position_3lop(boat,amer1,amer2,amer3):
+def compute_position_3lop(boat,mark1,mark2,mark3):
     """ Comput fix position with triangulation of 3LOP"""
     sigma = np.pi/90 # 2 degree
-    amer1.compute_bearing(boat,sigma)
-    amer2.compute_bearing(boat,sigma)
-    amer3.compute_bearing(boat,sigma)
-    intersection1 = compute_intersection(amer1,amer2)
-    intersection2 = compute_intersection(amer2,amer3)
-    intersection3 = compute_intersection(amer1,amer3)
+    mark1.compute_bearing(boat,sigma)
+    mark2.compute_bearing(boat,sigma)
+    mark3.compute_bearing(boat,sigma)
+    intersection1 = compute_intersection(mark1,mark2)
+    intersection2 = compute_intersection(mark2,mark3)
+    intersection3 = compute_intersection(mark1,mark3)
     #compute barycentre using Chasles formula
     barycentre = (intersection1 + intersection2 + intersection3)/3
     #circonscribed_circle does not work
@@ -148,26 +141,26 @@ def compute_position_3lop(boat,amer1,amer2,amer3):
              (intersection1[1], intersection2[1], intersection3[1], intersection1[1]),'g')
     return barycentre
 
-def compute_position_2lop(boat, amer1_up, amer2_up, show_lop):
+def compute_position_2lop(boat, mark1_up, mark2_up, show_lop):
     """ Compute estimated position with 2 LOP"""
-    amer1_down = Amer(amer1_up.position_x, amer1_up.position_y,0,"Amer1_down")
-    amer2_down = Amer(amer2_up.position_x, amer2_up.position_x,0,"Amer2_down")
+    mark1_down = Mark(mark1_up.position_x, mark1_up.position_y,0,"mark1_down")
+    mark2_down = Mark(mark2_up.position_x, mark2_up.position_x,0,"mark2_down")
 
     sigma = np.pi/90 # 2d egrees
-    amer1_up.compute_bearing(boat,sigma)
-    amer2_up.compute_bearing(boat,sigma)
-    amer1_down.compute_bearing(boat,-sigma)
-    amer2_down.compute_bearing(boat,-sigma)
+    mark1_up.compute_bearing(boat,sigma)
+    mark2_up.compute_bearing(boat,sigma)
+    mark1_down.compute_bearing(boat,-sigma)
+    mark2_down.compute_bearing(boat,-sigma)
     if show_lop:
-        amer1_up.plot_amer_bearing(boat)
-        amer2_up.plot_amer_bearing(boat)
-        amer1_down.plot_amer_bearing(boat)
-        amer2_down.plot_amer_bearing(boat)
+        mark1_up.plot_mark_bearing(boat)
+        mark2_up.plot_mark_bearing(boat)
+        mark1_down.plot_mark_bearing(boat)
+        mark2_down.plot_mark_bearing(boat)
 
-    inter1 = compute_intersection(amer1_up,amer2_up)
-    inter2 = compute_intersection(amer1_up,amer2_down)
-    inter3 = compute_intersection(amer1_down,amer2_down)
-    inter4 = compute_intersection(amer1_down,amer2_up)
+    inter1 = compute_intersection(mark1_up,mark2_up)
+    inter2 = compute_intersection(mark1_up,mark2_down)
+    inter3 = compute_intersection(mark1_down,mark2_down)
+    inter4 = compute_intersection(mark1_down,mark2_up)
 
     plt.plot( (inter1[0], inter2[0], inter3[0], inter4[0], inter1[0]),
             (inter1[1], inter2[1], inter3[1], inter4[1],  inter1[1]),'k')
@@ -181,16 +174,16 @@ def legend_unique():
     by_label = dict(zip(labels, handles))
     plt.legend(by_label.values(), by_label.keys())
 
-def get_best_amers(amer_table):
-    """ Get the three best amer from a set of amer"""
-    amer_table_size = len(amer_table)
-    bearing_table = np.zeros((amer_table_size ,amer_table_size ))
-    cost_table= np.zeros((amer_table_size ,amer_table_size ))
-    cost_table2= np.zeros((amer_table_size ,amer_table_size ))
+def get_best_marks(mark_table):
+    """ Get the three best mark from a set of mark"""
+    mark_table_size = len(mark_table)
+    bearing_table = np.zeros((mark_table_size ,mark_table_size ))
+    cost_table= np.zeros((mark_table_size ,mark_table_size ))
+    cost_table2= np.zeros((mark_table_size ,mark_table_size ))
 
-    for i , amer_i in enumerate(amer_table):
-        for j, amer_j in enumerate(amer_table):
-            bearing_table[i][j] = (amer_j.bearing - amer_i.bearing)
+    for i , mark_i in enumerate(mark_table):
+        for j, mark_j in enumerate(mark_table):
+            bearing_table[i][j] = (mark_j.bearing - mark_i.bearing)
 
     cost_table = bearing_table % (2*np.pi)
     cost_table2 = - bearing_table % (2*np.pi)
@@ -198,7 +191,7 @@ def get_best_amers(amer_table):
     cost_table = cost_table - (2*np.pi/3)
     cost_table = abs(cost_table)
 
-    comb = list(combinations(range(amer_table_size),3))
+    comb = list(combinations(range(mark_table_size),3))
 
     min_cost = 1000.0
     index_min = 0
@@ -210,8 +203,29 @@ def get_best_amers(amer_table):
             min_cost = cost
             index_min = i
 
-    amer_index = comb[index_min]
+    mark_index = comb[index_min]
 
-    return(amer_table[amer_index[0]], amer_table[amer_index[1]], amer_table[amer_index[2]])
+    return(mark_table[mark_index[0]], mark_table[mark_index[1]], mark_table[mark_index[2]])
 
 # %%
+if __name__ == "__main__":
+    plt.figure(1)
+    mark1 = Mark(100,300)
+    mark2 = Mark(500,500)
+    mark3 = Mark(500,100,0)
+    boat = Boat(300,310)
+    mark1.plot_position()
+    mark2.plot_position()
+    mark3.plot_position()
+
+    estimate = compute_position_3lop(boat,mark1,mark2,mark3)
+    boat.set_estimate(estimate)
+    boat.plot_boat()
+
+    mark1.plot_mark_bearing(boat)
+    mark2.plot_mark_bearing(boat)
+    mark3.plot_mark_bearing(boat)
+    legend_unique()
+
+    plt.title("3 LOP position fix")
+    plt.show()
