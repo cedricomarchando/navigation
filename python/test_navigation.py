a82dc76b6ed7131a6c08cc485ab66d27a194fb89
cmarchand <cedric.marchand@univ-ubs.fr>
2023-06-23 16:42:52 +0200

    rename amer to mark

--- python/test_navigation.py
+++ python/test_navigation.py
@@ -6,24 +6,22 @@ import navigation as nav
 
 
 # %%
-
-amer1 = nav.Amer(100,300)
-amer2 = nav.Amer(500,500)
-amer3 = nav.Amer(500,100,0)
+plt.figure(1)
+mark1 = nav.Mark(100,300)
+mark2 = nav.Mark(500,500)
+mark3 = nav.Mark(500,100,0)
 boat = nav.Boat(300,310)
-amer1.plot_position()
-amer2.plot_position()
-amer3.plot_position()
+mark1.plot_position()
+mark2.plot_position()
+mark3.plot_position()
 
-
-
-estimate = nav.compute_position_3lop(boat,amer1,amer2,amer3)
+estimate = nav.compute_position_3lop(boat,mark1,mark2,mark3)
 boat.set_estimate(estimate)
-boat.plot_position()
+boat.plot_boat()
 
-amer1.plot_amer_bearing(boat)
-amer2.plot_amer_bearing(boat)
-amer3.plot_amer_bearing(boat)
+mark1.plot_mark_bearing(boat)
+mark2.plot_mark_bearing(boat)
+mark3.plot_mark_bearing(boat)
 # circle1 = plt.Circle((position_x,position_y),radius, fill=False)
 # plt.gca().add_patch(circle1)
 
@@ -31,110 +29,92 @@ amer3.plot_amer_bearing(boat)
 nav.legend_unique()
 
 plt.title("3 LOP position fix")
-plt.show()
+plt.draw()
 
 
 # %%
-
-amer1 = nav.Amer(100,300)
-amer2 = nav.Amer(500,500)
-amer3 = nav.Amer(500,100)
-amer1.plot_position()
-amer2.plot_position()
-amer3.plot_position()
+plt.figure(2)
+mark1 = nav.Mark(100,300)
+mark2 = nav.Mark(500,500)
+mark3 = nav.Mark(500,100)
+mark1.plot_position()
+mark2.plot_position()
+mark3.plot_position()
 for i in range(40,600,50):
     for j in range(150,590,100):
         boat = nav.Boat(i,j)
-        estimate = nav.compute_position_3lop(boat,amer1,amer2,amer3)
+        estimate = nav.compute_position_3lop(boat,mark1,mark2,mark3)
         boat.set_estimate(estimate)
-        boat.plot_position()
+        boat.plot_boat()
         del boat
 nav.legend_unique()
 plt.title("3 LOP position fix")
-plt.show()
+plt.draw()
 
-# %%
-
-amer1 = nav.Amer(100,300)
-amer2 = nav.Amer(400,100)
-amer3 = nav.Amer(500,100)
-amer1.plot_position()
-amer2.plot_position()
-amer3.plot_position()
-
-for i in range(140,500,100):
-    for j in range(120,500,100):
-        boat = nav.Boat(i,j)
-        estimate = nav.compute_position_3lop(boat,amer1,amer2,amer3)
-        boat.set_estimate(estimate)
-        boat.plot_position()
-        del boat
-
-nav.legend_unique()
-plt.title("3 LOP position fix")
-plt.show()
 
 # %% 2 LOP intersection
-
-amer1_up = nav.Amer(100,100)
-amer2_up = nav.Amer(500,500)
-amer1_up.plot_position()
-amer2_up.plot_position()
+plt.figure(4)
+mark1_up = nav.Mark(100,100)
+mark2_up = nav.Mark(500,500)
+mark1_up.plot_position()
+mark2_up.plot_position()
 
 boat = nav.Boat(400,310)
-boat.plot_position()
+boat.plot_boat()
 
-nav.compute_position_2lop(boat,amer1_up,amer2_up,show_lop=True)
+nav.compute_position_2lop(boat,mark1_up,mark2_up,show_lop=True)
 
 nav.legend_unique()
 plt.title("2 LOP fix")
-plt.show()
+plt.draw()
 
 # %%
-amer1_up = nav.Amer(100,100)
-amer2_up = nav.Amer(500,500)
-amer1_up.plot_position()
-amer2_up.plot_position()
+plt.figure(5)
+mark1_up = nav.Mark(100,100)
+mark2_up = nav.Mark(500,500)
+mark1_up.plot_position()
+mark2_up.plot_position()
 boat = nav.Boat(400,310)
-boat.plot_position()
+boat.plot_boat()
 
 for i in range(100,600,50):
     for j in range(-100,i,50):
         boat = nav.Boat(i,j)
-        boat.plot_position()
-        nav.compute_position_2lop(boat,amer1_up,amer2_up,show_lop=False)
+        boat.plot_boat()
+        nav.compute_position_2lop(boat,mark1_up,mark2_up,show_lop=False)
         del boat
 nav.legend_unique()
 plt.title("2 LOP position fix")
-plt.show()
+plt.draw()
 
 # %%
+plt.figure(6)
 sigma = np.pi/90 # 2 degree
 
-amer1 = nav.Amer(100.0, 500.0)
-amer2 = nav.Amer(500.0, 500.0)
-amer3 = nav.Amer(500.0, 100.0)
-amer4 = nav.Amer(100.0, 100.0)
-amer5 = nav.Amer(250.0, 510.0)
-amer1.plot_position()
-amer2.plot_position()
-amer3.plot_position()
-amer4.plot_position()
-amer5.plot_position()
-amer_table=[amer1, amer2, amer3, amer4, amer5]
+mark1 = nav.Mark(100.0, 500.0)
+mark2 = nav.Mark(500.0, 500.0)
+mark3 = nav.Mark(500.0, 100.0)
+mark4 = nav.Mark(100.0, 100.0)
+mark5 = nav.Mark(250.0, 510.0)
+mark1.plot_position()
+mark2.plot_position()
+mark3.plot_position()
+mark4.plot_position()
+mark5.plot_position()
+mark_table=[mark1, mark2, mark3, mark4, mark5]
 
 for i in range(150,500,100):
     for j in range(150,500,100):
         boat = nav.Boat(i, j)
-        for amer in amer_table:
-            amer.compute_bearing(boat,sigma)
-        amerA, amerB, amerC = nav.get_best_amers(amer_table)
-        estimate = nav.compute_position_3lop(boat,amerA,amerB,amerC)
+        for mark in mark_table:
+            mark.compute_bearing(boat,sigma)
+        markA, markB, markC = nav.get_best_marks(mark_table)
+        estimate = nav.compute_position_3lop(boat,markA,markB,markC)
         boat.set_estimate(estimate)
-        boat.plot_position()
-        amerA.plot_amer_bearing(boat)
-        amerB.plot_amer_bearing(boat)
-        amerC.plot_amer_bearing(boat)
+        boat.plot_boat()
+        markA.plot_mark_bearing(boat)
+        markB.plot_mark_bearing(boat)
+        markC.plot_mark_bearing(boat)
 
 nav.legend_unique()
 plt.show()
