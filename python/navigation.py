cc6baf8a94ead996d82050fd9b4984a8c8133bcf
cmarchand <cedric.marchand@univ-ubs.fr>
2023-07-03 16:59:27 +0200

    put compute_position_3lop and 2lop in BoatSimu class

--- python/navigation.py
+++ python/navigation.py
@@ -20,6 +20,76 @@ class BoatSimu:
         self.boat_true.plot_boat("True boat")
         self.boat_estimate.plot_boat("Estimated boat")
 
+    def compute_position_3lop(self, mark1, mark2, mark3):
+        """ Comput fix position with triangulation of 3 Lines Of Position (LOP) """
+        sigma = np.pi/90 # 2 degree
+        mark1.compute_bearing(self.boat_true,sigma)
+        mark2.compute_bearing(self.boat_true,sigma)
+        mark3.compute_bearing(self.boat_true,sigma)
+        intersection1 = compute_intersection(mark1, mark2)
+        intersection2 = compute_intersection(mark2, mark3)
+        intersection3 = compute_intersection(mark1, mark3)
+        #compute barycentre using Chasles formula
+        barycentre = (intersection1 + intersection2 + intersection3)/3
+        #circonscribed_circle does not work
+        plt.plot( (intersection1[0], intersection2[0], intersection3[0], intersection1[0]),
+                (intersection1[1], intersection2[1], intersection3[1], intersection1[1]),'g')
+        self.boat_estimate.set_position(barycentre)
+        return barycentre
+
+    def compute_position_2lop(self, mark1_up, mark2_up, show_lop):
+        """ Compute estimated position with 2 LOP"""
+        mark1_down = Mark(mark1_up.position_x, mark1_up.position_y)
+        mark2_down = Mark(mark2_up.position_x, mark2_up.position_x)
+
+        sigma = np.pi/90 # 2d egrees
+        mark1_up.compute_bearing(self.boat_true,sigma)
+        mark2_up.compute_bearing(self.boat_true,sigma)
+        mark1_down.compute_bearing(self.boat_true,-sigma)
+        mark2_down.compute_bearing(self.boat_true,-sigma)
+        if show_lop:
+            mark1_up.plot_mark_bearing(self.boat_true)
+            mark2_up.plot_mark_bearing(self.boat_true)
+            mark1_down.plot_mark_bearing(self.boat_true)
+            mark2_down.plot_mark_bearing(self.boat_true)
+
+        inter1 = compute_intersection(mark1_up,mark2_up)
+        inter2 = compute_intersection(mark1_up,mark2_down)
+        inter3 = compute_intersection(mark1_down,mark2_down)
+        inter4 = compute_intersection(mark1_down,mark2_up)
+
+        plt.plot( (inter1[0], inter2[0], inter3[0], inter4[0], inter1[0]),
+                (inter1[1], inter2[1], inter3[1], inter4[1],  inter1[1]),'k')
+        plt.fill( (inter1[0], inter2[0], inter3[0], inter4[0], inter1[0]),
+                (inter1[1], inter2[1], inter3[1], inter4[1],  inter1[1]),'g',label="position area")
+        
+        barycentre = (inter1 + inter2 + inter3 + inter4)/4
+        self.boat_estimate.set_position(barycentre)
+        return barycentre
+    
+    def run_fix(self, mark, duration, sigma):
+        """ Run fix: get position from 1 mark and speed """
+        mark.compute_bearing(self.boat_true, sigma)
+        save_bearing = mark.bearing
+        # compute updated bearing after running
+        self.boat_true.run(duration)
+        mark.compute_bearing(self.boat_true, sigma)
+        mark.plot_mark_bearing(self.boat_true)
+        # run mark in the direction of the boat
+        duration = 1
+        mark_tmp = Mark(
+            mark.position_x + self.boat_estimate.speed * duration * np.sin(self.boat_estimate.course),
+            mark.position_y + self.boat_estimate.speed * duration * np.cos(self.boat_estimate.course),
+            bearing = save_bearing
+            )
+        plt.plot(mark_tmp.position_x, mark_tmp.position_y,'+k', label ="mark shifted")
+
+        mark_tmp.plot_mark_bearing(self.boat_true)
+
+        estimate = compute_intersection(mark,mark_tmp)
+        del mark_tmp
+        self.boat_estimate.set_position(estimate)
+        return estimate
 
 class Boat:
     """ Boat class """
@@ -63,28 +133,6 @@ class Boat:
         self.true_x = position[0]
         self.true_y = position[1]
 
-    def run_fix(self,mark,duration,sigma):
-        """ Run fix: get position from 1 mark and speed """
-        save_bearing = mark.bearing
-        # compute mark bearing after running
-        self.run(duration)
-        mark.compute_bearing(self, sigma)
-        mark.plot_mark_bearing(self)
-        # run mark in the direction of the boat
-        duration = 1
-        mark_tmp = Mark(
-            mark.position_x + self.speed * duration * np.sin(self.course),
-            mark.position_y + self.speed * duration * np.cos(self.course),
-            save_bearing
-            )
-        plt.plot(mark_tmp.position_x, mark_tmp.position_y,'+k', label ="mark shifted")
-
-        mark_tmp.plot_mark_bearing(self)
-
-        estimate = compute_intersection(mark,mark_tmp)
-        del mark_tmp
-        return estimate
-
 
 class Mark:
     """ Mark class, including landmarks and Seamarks """
@@ -94,15 +142,14 @@ class Mark:
         self.position_y = position_y
         self.bearing = bearing
         self.mark_type = mark_type.lower()
-        if top_mark_type is not None:
-            self.top_mark_type = top_mark_type
+        self.top_mark_type = top_mark_type
         self.name = name
         Mark.number_of_marks += 1
 
     def plot_position(self):
         """ Plot position"""
         #plt.plot(self.position_x, self.position_y,'*k', markersize = 10, label ="Mark")
-        marker.PlotMark( self.position_x, self.position_y, self.mark_type  )
+        marker.PlotMark( self.position_x, self.position_y, self.mark_type, self.top_mark_type  )
 
     def plot_mark_bearing(self, boat):
         """ Plot LOP of a mark with dotted line"""
@@ -129,48 +176,6 @@ def compute_intersection(mark1, mark2):
     intersection = np.array([intersection_x, intersection_y])
     return intersection 
 
-
-def compute_position_3lop(boat, mark1, mark2, mark3):
-    """ Comput fix position with triangulation of 3 Lines Of Position (LOP) """
-    sigma = np.pi/90 # 2 degree
-    mark1.compute_bearing(boat,sigma)
-    mark2.compute_bearing(boat,sigma)
-    mark3.compute_bearing(boat,sigma)
-    intersection1 = compute_intersection(mark1, mark2)
-    intersection2 = compute_intersection(mark2, mark3)
-    intersection3 = compute_intersection(mark1, mark3)
-    #compute barycentre using Chasles formula
-    barycentre = (intersection1 + intersection2 + intersection3)/3
-    #circonscribed_circle does not work
-    plt.plot( (intersection1[0], intersection2[0], intersection3[0], intersection1[0]),
-             (intersection1[1], intersection2[1], intersection3[1], intersection1[1]),'g')
-    return barycentre
-
-def compute_position_2lop(boat, mark1_up, mark2_up, show_lop):
-    """ Compute estimated position with 2 LOP"""
-    mark1_down = Mark(mark1_up.position_x, mark1_up.position_y)
-    mark2_down = Mark(mark2_up.position_x, mark2_up.position_x)
-
-    sigma = np.pi/90 # 2d egrees
-    mark1_up.compute_bearing(boat,sigma)
-    mark2_up.compute_bearing(boat,sigma)
-    mark1_down.compute_bearing(boat,-sigma)
-    mark2_down.compute_bearing(boat,-sigma)
-    if show_lop:
-        mark1_up.plot_mark_bearing(boat)
-        mark2_up.plot_mark_bearing(boat)
-        mark1_down.plot_mark_bearing(boat)
-        mark2_down.plot_mark_bearing(boat)
-
-    inter1 = compute_intersection(mark1_up,mark2_up)
-    inter2 = compute_intersection(mark1_up,mark2_down)
-    inter3 = compute_intersection(mark1_down,mark2_down)
-    inter4 = compute_intersection(mark1_down,mark2_up)
-
-    plt.plot( (inter1[0], inter2[0], inter3[0], inter4[0], inter1[0]),
-            (inter1[1], inter2[1], inter3[1], inter4[1],  inter1[1]),'k')
-    plt.fill( (inter1[0], inter2[0], inter3[0], inter4[0], inter1[0]),
-            (inter1[1], inter2[1], inter3[1], inter4[1],  inter1[1]),'g',label="position area")
 
 
 def legend_unique():
@@ -229,8 +234,7 @@ if __name__ == "__main__":
     mark2.plot_position()
     mark3.plot_position()
 
-    estimate = compute_position_3lop(boat_simu.boat_true,mark1,mark2,mark3)
-    boat_simu.boat_estimate.set_position(estimate)
+    boat_simu.compute_position_3lop(mark1,mark2,mark3)
     boat_simu.plot_boat()
 
     mark1.plot_mark_bearing(boat_simu.boat_true)
