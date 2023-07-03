ac118f54f2ff74a32bc30d4fd4199e705d4d1f23
cmarchand <cedric.marchand@univ-ubs.fr>
2023-07-03 17:03:25 +0200

    use run_fix as method of BoatSimu class

--- python/test_running_fix.py
+++ python/test_running_fix.py
@@ -7,57 +7,60 @@ import navigation as nav
 # %%
 
 # initialisation
-amer = nav.Amer(200,500,0,"Amer1")
-amer.plot_position()
-boat = nav.Boat(50,50, course=np.pi/4, speed=100)
-boat.plot_position()
+plt.figure(1)
+mark = nav.Mark(200,500,'church')
+mark.plot_position()
+boat_simu = nav.BoatSimu(50,50)
+boat_simu.boat_true.course=np.pi/2
+boat_simu.boat_true.speed=100
+boat_simu.boat_estimate.course=np.pi/2
+boat_simu.boat_estimate.speed=100
+boat_simu.plot_boat()
 
 sigma = np.pi/180
-amer.compute_bearing(boat, sigma)
-amer.plot_amer_bearing(boat)
+mark.compute_bearing(boat_simu.boat_true, 0)
+mark.plot_mark_bearing(boat_simu.boat_true)
 
 for i in range(8):
-    estimate = boat.run_fix(amer,1,sigma)
-    boat.set_estimate(estimate)
-    boat.plot_position()
-
+    boat_simu.run_fix(mark,1,sigma)
+    boat_simu.plot_boat()
 
 nav.legend_unique()
-plt.title("Runnig fix")
-plt.show()
+plt.title("Running fix")
+plt.draw()
 
 
 # %%
-
+plt.figure(2)
 sigma = 0 # 2 degree
 
-amer1 = nav.Amer(100.0, 500.0)
-amer2 = nav.Amer(250.0, 510.0)
-amer3 = nav.Amer(500.0, 500.0)
-amer4 = nav.Amer(500.0, 100.0)
-amer5 = nav.Amer(200.0, 100.0)
-amer6 = nav.Amer(50.0, 150.0)
+mark1 = nav.Mark(100.0, 500.0, 'church')
+mark2 = nav.Mark(250.0, 510.0, 'lighthouse')
+mark3 = nav.Mark(500.0, 500.0, 'major_lighthouse')
+mark4 = nav.Mark(500.0, 100.0, 'land_tower')
+mark5 = nav.Mark(200.0, 100.0, 'water_tower')
+mark6 = nav.Mark(50.0, 150.0, 'tower', 'east')
 
-amer1.plot_position()
-amer2.plot_position()
-amer3.plot_position()
-amer4.plot_position()
-amer5.plot_position()
-amer6.plot_position()
-amer_table=[amer1, amer2, amer3, amer4, amer5, amer6]
+amer_table=[mark1, mark2, mark3, mark4, mark5, mark6]
+for mark in amer_table:
+    mark.plot_position()
 
+boat_simu = nav.BoatSimu(200,300)
+boat_simu.boat_true.course=np.pi/2
+boat_simu.boat_true.speed=100
+boat_simu.boat_estimate.course=np.pi/2
+boat_simu.boat_estimate.speed=100
 
-boat = nav.Boat(200,300, course=np.pi/2, speed=100)
-boat.plot_position()
-boat.plot_speed()
+boat_simu.plot_boat()
+boat_simu.boat_true.plot_speed()
 
 # build cost table such that lowest cost is + or - pi/2 angle
 bearing_table = np.zeros((len(amer_table),1),dtype=float)
 for i, amer in enumerate(amer_table):
-    amer.compute_bearing(boat,sigma)
+    amer.compute_bearing(boat_simu.boat_true,sigma)
     bearing_table[i]=amer.bearing
 
-bearing_table = boat.course - bearing_table
+bearing_table = boat_simu.boat_true.course - bearing_table
 print(bearing_table)
 # bearing_table = abs(bearing_table)
 # print(bearing_table)
