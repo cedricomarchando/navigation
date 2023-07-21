b82d579654d3e22367dde364c81aa29a3134059a
cmarchand <cedric.marchand@univ-ubs.fr>
2023-07-21 16:14:52 +0200

    add track

--- python/test_running_fix.py
+++ python/test_running_fix.py
@@ -4,39 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import navigation as nav
 
-# %%
-
-# initialisation
-plt.figure(1)
-mark = nav.Mark([200,500],'church')
-mark.plot_mark()
-boat_simu = nav.BoatSimu([50,50])
-boat_simu.boat_true.course=np.pi/2
-boat_simu.boat_true.speed=100
-boat_simu.boat_estimate.course=np.pi/2
-boat_simu.boat_estimate.speed=100
-boat_simu.plot_boat()
-
-sigma = np.pi/180
-mark.compute_bearing(boat_simu.boat_true, 0)
-mark.plot_mark_bearing(boat_simu.boat_true)
-
-for i in range(8):
-    boat_simu.run_fix(mark,1,sigma)
-    boat_simu.plot_boat()
-
-nav.legend_unique()
-plt.title("Running fix")
-plt.draw()
-
-
-# %%
 plt.figure(2)
-sigma = 0 # 2 degree
+sigma =  np.pi/180 # 2 degree
 
-mark1 = nav.Mark([100.0, 500.0], 'church')
+mark1 = nav.Mark([100.0, 400.0], 'church')
 mark2 = nav.Mark([250.0, 510.0], 'lighthouse')
-mark3 = nav.Mark([500.0, 500.0], 'major_lighthouse')
+mark3 = nav.Mark([700.0, 400.0], 'major_lighthouse')
 mark4 = nav.Mark([500.0, 100.0], 'land_tower')
 mark5 = nav.Mark([200.0, 100.0], 'water_tower')
 mark6 = nav.Mark([50.0, 150.0], 'tower', 'east')
@@ -45,22 +18,29 @@ mark_table=[mark1, mark2, mark3, mark4, mark5, mark6]
 for mark in mark_table:
     mark.plot_mark()
 
-boat_simu = nav.BoatSimu([10,300])
-boat_simu.boat_true.course=np.pi/2
-boat_simu.boat_true.speed=100
-boat_simu.boat_estimate.course=np.pi/2
-boat_simu.boat_estimate.speed=100
+boat_simu = nav.BoatSimu([100,300],[100,300])
+boat_simu.boat_true.ground_track.course = np.pi/2
+boat_simu.boat_true.ground_track.speed = 100
+boat_simu.boat_estimate.ground_track.course = np.pi/2
+boat_simu.boat_estimate.ground_track.speed = 100
+
+
+fix_period = 1
+print(boat_simu.boat_true.ground_track)
+boat_simu.boat_true.ground_track.plot_track()
 
 boat_simu.plot_boat()
-boat_simu.boat_true.plot_speed()
 
 for i in range(8):
-    best_mark = nav.get_best_mark90( boat_simu.boat_true, mark_table)
-    boat_simu.run_fix(best_mark,1,sigma)
+    best_mark = boat_simu.get_1best_mark(mark_table, fix_period)
+    boat_simu.run_fix(best_mark, fix_period ,sigma, True)
+    #boat_simu.run_fix(mark3, fix_period, sigma, False)
     boat_simu.plot_boat()
 
 
-print(best_mark.mark_type)
+# boat_simu.run_fix(mark1,1,sigma)
+# boat_simu.plot_boat()
+
 plt.show()
 
 
