08a6d820b67919a8023ec35e77abbc26dc50ac97
cmarchand <cedric.marchand@univ-ubs.fr>
2023-08-24 15:50:03 +0200

    add constant, enumerate, reorgenize

--- python/test_cartopy.py
+++ python/test_cartopy.py
@@ -35,33 +35,37 @@ marks_map = nav.MarksMap()
 marks_map.marks_csv('marks.csv')
 marks_map.plot_map()
 
-route = nav.Route()
-route.route_csv('./route.csv')
-route.plot_route()
+routes = nav.Route()
+routes.route_csv('./route.csv')
+routes.plot_route()
 
-print(route.route[0])
+print(routes.route[0])
 
 sigma = np.pi/90 # 2 degree
 
 
-speed = 0.1
-fix_period = 0.01
+SPEED = 0.1
+FIX_PERIOD = 0.01
 
-boat_simu = nav.BoatSimu(route.route[0].position, route.route[0].position)
+boat_simu = nav.BoatSimu(routes.route[0].position, routes.route[0].position)
 
-boat_simu.compute_waypoint_distance(route.route[1])
+boat_simu.set_tide_track(course=np.pi, speed=SPEED/3)
+
+boat_simu.compute_waypoint_distance(routes.route[1])
 print(f'distance_estimate:{boat_simu.boat_estimate.waypoint_distance} \t distance_true:{boat_simu.boat_true.waypoint_distance}')
-boat_simu.boat_true.ground_track.speed = speed
-boat_simu.boat_estimate.ground_track.speed = speed
+boat_simu.boat_true.water_track.speed = SPEED
+boat_simu.boat_estimate.water_track.speed = SPEED
+boat_simu.boat_true.ground_track.speed = SPEED
+boat_simu.boat_estimate.ground_track.speed = SPEED
 boat_simu.plot_boat()
 nearest_marks = boat_simu.select_near_fixed_marks(marks_map, sigma, 6)
 boat_simu.update_2lop_fix(nearest_marks)
 
-for i in range(1, len(route.route)):
-    boat_simu.go_to_waypoint(route.route[i], marks_map, sigma, fix_period, nav.FixType.FIX_3LOP)
+for route in routes.route:
+    boat_simu.go_to_waypoint(route, marks_map, sigma, FIX_PERIOD, nav.FixType.FIX_3LOP)
 
 
-#boat_simu.go_to_waypoint(route.route[2], marks_map, sigma, fix_period, nav.FixType.FIX_3LOP)
+#boat_simu.go_to_waypoint(route.route[2], marks_map, sigma, FIX_PERIOD, nav.FixType.FIX_3LOP)
 
 plt.show()
 # %%
