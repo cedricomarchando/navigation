303627ba4ccbba3bd2cafe0f873fbbeaaec96d26
cmarchand <cedric.marchand@univ-ubs.fr>
2023-09-14 11:12:46 +0200

    add water_track course

--- python/test_running_fix.py
+++ python/test_running_fix.py
@@ -11,7 +11,7 @@ mark1 = nav.Mark([100.0, 400.0], 'church')
 mark2 = nav.Mark([250.0, 510.0], 'lighthouse')
 mark3 = nav.Mark([700.0, 400.0], 'major_lighthouse')
 mark4 = nav.Mark([500.0, 100.0], 'land_tower')
-mark5 = nav.Mark([200.0, 100.0], 'water_tower')
+mark5 = nav.Mark([200.0, 100.0], 'pillar','green')
 mark6 = nav.Mark([50.0, 150.0], 'tower', 'east')
 
 mark_table=[mark1, mark2, mark3, mark4, mark5, mark6]
@@ -21,8 +21,10 @@ for mark in mark_table:
 boat_simu = nav.BoatSimu([100,300],[100,300])
 boat_simu.boat_true.ground_track.course = np.pi/2
 boat_simu.boat_true.ground_track.speed = 100
+boat_simu.boat_true.water_track.course = np.pi/2
 boat_simu.boat_estimate.ground_track.course = np.pi/2
 boat_simu.boat_estimate.ground_track.speed = 100
+boat_simu.boat_estimate.water_track.course = np.pi/2
 
 
 fix_period = 1
@@ -34,7 +36,7 @@ boat_simu.plot_boat()
 for i in range(8):
     best_mark = boat_simu.get_1best_mark(mark_table, fix_period)
     boat_simu.run_fix(best_mark, fix_period ,sigma, True)
-    #boat_simu.run_fix(mark3, fix_period, sigma, False)
+    #boat_simu.run_fix(mark3, fix_period, sigma, True)
     boat_simu.plot_boat()
 
 
